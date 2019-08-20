6fcc824a0f4f8aa63679a6274b438e6721045efc
Tom Eastman <tom@eastman.net.nz>
2019-08-20 17:12:25 +1200

    Add code coverage to tests.

--- secateur/tests/test_forms.py
+++
@@ -1,5 +0,0 @@
-from secateur import forms
-
-
-def test_block_accounts_form():
-    form = forms.BlockAccountsForm()
--- secateur/tests/test_views.py
+++ secateur/tests/test_views.py
@@ -1,9 +1,18 @@
-def test_home(client, db):
-    r = client.get("/")
+import pytest
+from unittest import mock
+
+def test_home(client):
+    with mock.patch('request.models.Request.from_http_request') as m:
+        r = client.get("/")
     assert r.status_code == 200
 
 
-def test_block(client, admin_user):
+def test_get_block(client, admin_user):
     client.login(username="admin", password="password")
     r = client.get("/block/")
     assert r.status_code == 200
+
+
+def test_imports():
+    import secateur.wsgi
+    import secateur.apps
