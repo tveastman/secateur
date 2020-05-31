3d53ec3043e43ad461557003c4561324163285cf
Tom Eastman <tom@eastman.nz>
2020-05-31 15:47:11 +1200

    Update the block form to allow people to be blocked forever.

--- secateur/forms.py
+++ secateur/forms.py
@@ -6,13 +6,22 @@ class Disconnect(forms.Form):
 
 
 class BlockAccountsForm(forms.Form):
+    DURATION_CHOICES = (
+        (3, "Three weeks"),
+        (6, "Six weeks"),
+        (12, "Three months"),
+        (26, "Six months"),
+        (52, "One year"),
+        (None, "Forever")
+    )
     screen_name = forms.CharField(help_text="The Twitter username.")
     duration = forms.IntegerField(
         min_value=1,
         max_value=52,
         initial=6,
-        help_text="How long to block the accounts (in weeks)",
+        help_text="How long do you want the block to last?",
         required=False,
+        widget=forms.RadioSelect(choices=DURATION_CHOICES)
     )
     block_account = forms.BooleanField(required=False)
     mute_account = forms.BooleanField(required=False)
