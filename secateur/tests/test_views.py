4edaf0996b75902c52cb67e6d4660e2c312d9979
nzdjb <github@2a1b.com>
2022-07-02 20:15:17 +1200

    test: Add block test.

--- secateur/tests/test_views.py
+++ secateur/tests/test_views.py
@@ -9,6 +9,13 @@ def test_home(client: django.test.client.Client) -> None:
     assert r.status_code == 200
 
 
+def test_block(client: django.test.client.Client) -> None:
+    with mock.patch("request.models.Request.from_http_request"):
+        r = client.get("/block/")
+    assert r.status_code == 302
+    assert r.headers["Location"] == "/login/twitter/?next=/block/"
+
+
 def test_imports() -> None:
     import secateur.wsgi
     import secateur.apps
