d19f094c56ee4c8a8bd7ba00d1d10113a6247fd0
nzdjb <github@2a1b.com>
2022-07-02 20:16:28 +1200

    test: Add test for block page with user.

--- secateur/tests/test_views.py
+++ secateur/tests/test_views.py
@@ -16,6 +16,28 @@ def test_block(client: django.test.client.Client) -> None:
     assert r.headers["Location"] == "/login/twitter/?next=/block/"
 
 
+@pytest.mark.django_db
+def test_block_with_user(client: django.test.client.Client) -> None:
+    u = _test_user()
+    client.force_login(u)
+    with mock.patch("request.models.Request.from_http_request"):
+        r = client.get("/block/")
+    assert r.status_code == 200
+
+
 def test_imports() -> None:
     import secateur.wsgi
     import secateur.apps
+
+
+def _test_user() -> models.User():
+    account = models.Account(
+        user_id = 1
+    )
+    account.save()
+    user = models.User(
+        account=account
+    )
+    user.save()
+    return user
+
