31d72cc1091b3c564c4d77f6c58de90476eb404b
nzdjb <github@2a1b.com>
2022-07-02 20:53:53 +1200

    test: Basic no-user test for a bunch more views.

--- secateur/tests/test_views.py
+++ secateur/tests/test_views.py
@@ -10,6 +10,13 @@ def test_home(client: django.test.client.Client) -> None:
     assert r.status_code == 200
 
 
+def test_admin(client: django.test.client.Client) -> None:
+    with mock.patch("request.models.Request.from_http_request"):
+        r = client.get("/admin/")
+    assert r.status_code == 302
+    assert r.headers["Location"] == "/admin/login/?next=/admin/"
+
+
 def test_block(client: django.test.client.Client) -> None:
     _check_redirect_when_not_logged_in(client, "block")
 
@@ -22,9 +29,50 @@ def test_block_with_user(client: django.test.client.Client) -> None:
         r = client.get("/block/")
     assert r.status_code == 200
 
+# Missing?
+# def test_blocked(client: django.test.client.Client) -> None:
+#     _check_redirect_when_not_logged_in(client, "blocked")
+
+
+def test_unblock_everybody(client: django.test.client.Client) -> None:
+    _check_redirect_when_not_logged_in(client, "unblock-everybody")
+
+
+def test_search(client: django.test.client.Client) -> None:
+    _check_redirect_when_not_logged_in(client, "search")
+
 
 def test_log_messages(client: django.test.client.Client) -> None:
     _check_redirect_when_not_logged_in(client, "log-messages")
+
+
+def test_block_messages(client: django.test.client.Client) -> None:
+    _check_redirect_when_not_logged_in(client, "block-messages")
+
+
+@pytest.mark.django_db
+def test_logout(client: django.test.client.Client) -> None:
+    with mock.patch("request.models.Request.from_http_request"):
+        r = client.get("/logout/")
+    assert r.status_code == 302
+    assert r.headers["Location"] == "/"
+
+
+def test_disconnect(client: django.test.client.Client) -> None:
+    _check_redirect_when_not_logged_in(client, "disconnect")
+
+
+# Behaves differently
+# def test_disconnected(client: django.test.client.Client) -> None:
+#     _check_redirect_when_not_logged_in(client, "disconnected")
+
+
+def test_following(client: django.test.client.Client) -> None:
+    _check_redirect_when_not_logged_in(client, "following")
+
+
+def test_update_following(client: django.test.client.Client) -> None:
+    _check_redirect_when_not_logged_in(client, "update-following")
 
 
 def test_imports() -> None:
